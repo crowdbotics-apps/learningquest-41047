89639ce2ae99642ff74d6aaeedbfe00cee60209c
Crowdbotics <team@crowdbotics.com>
2023-05-28 00:04:09 +0000

    Initial commit.

---
+++ learningquest_41047/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for learningquest_41047 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learningquest_41047.settings')
+
+application = get_wsgi_application()
